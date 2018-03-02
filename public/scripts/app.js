99af2635472fca3886479aac0c195b0c2eece95c
Rafael Fontoura <rafael.fontoura@gmail.com>
2018-03-01 22:00:42 -0300

    Criação da estrutura inicial da aplicação
    1. Criação do `index.html` e app.js, instalação do live-server
    globalmente

---
+++ public/scripts/app.js
@@ -0,0 +1,7 @@
+console.log('App.js tá executando!');
+
+// JSX - JavaScript XML
+//var template = <p>This is JSX from app.js!</p>;
+//var appRoot = document.getElementById('');
+
+//ReactDOM.render(template, appRoot);
