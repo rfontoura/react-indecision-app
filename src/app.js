c1f489721f998dafbd1a46883369b500f8a97896
Rafael Fontoura <rafael.fontoura@gmail.com>
2018-03-23 22:11:28 -0300

    Utilizando Javascript expressions no JSX
    1. Utilizando javascript expressions na JSX (entre chaves). Pode-se utilizar qualquer expressão Javascript na expressão (funções, operadores, acesso a atributos de objetos com notação de ponto)
    2. Criando objeto e utilizando seus atributos na JSX

--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
 console.log('App.js tá executando!');
 
-// JSX - JavaScript XML
-var template = <h1>Indecisão App</h1>;
-var appRoot = document.getElementById('app');
+var user = {
+    name: 'Rafaelo',
+    age: 135,
+    location: 'GYN'
+};
 
-ReactDOM.render(template, appRoot);
+var app = {
+    title: 'Minha Grandissíssima Aplicação',
+    subtitle: 'Como fazer uma aplicação Hello World perfeita',
+    tags: ['shit', 'simple', 'mastering']
+};
+
+// JSX - JavaScript XML
+var template2 = (
+    <div>
+        <h1>{app.title}</h1>
+        <p>App, what for is it?: {app.subtitle}</p>
+        <p>Tags: {app.tags[0]}, {app.tags[1]}, {app.tags[2]}</p>
+    </div>
+);
+
+var domElement = document.getElementById('app');
+ReactDOM.render(template2, domElement);
