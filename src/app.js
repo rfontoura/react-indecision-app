2120e0549bfd25acc8b1e22f2f5bbeafa4421298
Rafael Fontoura <rafael.fontoura@gmail.com>
2018-04-11 22:47:14 -0300

    Criando JSX de forma condicional
    1. Usando funções e retornando JSX
    2. Utilizando operador ternário
    3. Utilizando expressão booleana com valor JSX

--- src/app.js
+++ src/app.js
@@ -1,29 +1,51 @@
 console.log('App.js tá executando!');
 
 var user = {
-    name: 'Chaves do Oito',
-    age: 12,
+    /*name: 'Chaves do Oito',*/
+    age: 18,
     location: 'Vila, México'
 };
 
 var app = {
-    title: 'Minha Grandissíssima Aplicaçãoadsf',
-    subtitle: 'Como fazer uma aplicação Hello World perfeita',
-    tags: ['shit', 'simpled', 'mastering']
+    title: 'Minha Grandissíssima Aplicação',
+    subtitle: 'Como fazer uma aplicação Hello World',
+    options: ['Um', 'Dois', 'Três', 'Quatro']
 };
 
 function getLocation(location) {
-    return location ? location : '(desconhecida)';
+    if (location) {
+        return <p>Localização: {location}</p>
+    }
+
+    return undefined;
+}
+
+function getOptions(optionsArray) {
+    var itens = [];
+    for (var opcao in optionsArray) {
+        itens.push(<li>{optionsArray[opcao]}</li>);
+    }
+
+    return <ul>{itens}</ul>;
 }
 
 // JSX - JavaScript XML
 var template2 = (
     <div>
-        <h1>{user.name}</h1>
-        <p>Idade: {user.age} anos</p>
-        <p>Localização: {getLocation(user.location)}</p>
+        <h1>{user.name ? user.name : 'Anônimo'}</h1>
+        {user.age >= 18 && <p>Idade: {user.age} anos</p>}
+        {getLocation(user.location)}
+    </div>
+);
+
+var template3 = (
+    <div>
+        <h1>{app.title}</h1>
+        {app.subtitle && <h2>{app.subtitle}</h2>}
+        <p>{!app.options || app.options.length === 0 ? 'No options' : 'Here are your options:'}</p>
+        {app.options.length > 0 && getOptions(app.options)}
     </div>
 );
 
 var domElement = document.getElementById('app');
-ReactDOM.render(template2, domElement);
+ReactDOM.render(template3, domElement);
