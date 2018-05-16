4f277751c8f12d10d76055f147cd44591883050e
Rafael Fontoura <rafael.fontoura@gmail.com>
2018-05-15 22:49:22 -0300

    Trabalhando com parâmetros de componentes
    1. Parametrização de componentes utilizando o atributo `this.props`
    2. Encadeamento de parâmetros e inclusão de componentes compostos de componentes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 class IndecisionApp extends React.Component {
     render() {
+
+        const titulo = 'Indecisão';
+        const subtitulo = 'Coloque sua vida nas mãos de um computador';
+        const opcoes = ['Opção 1', 'Opção dois', 'Opção IV'];
+
         return (
             <div>
-                <Header />
+                <Header title={titulo} subtitle={subtitulo} />
                 <Action />
-                <Options />
+                <Options values={opcoes}/>
                 <AddOption />
             </div>
         );
@@ -15,8 +20,8 @@ class Header extends React.Component {
     render() {
         return (
             <div>
-            <h1>Aplicação da Decisão</h1>
-            <h2>Colocando sua vida nas mãos de um computador</h2>
+            <h1>{this.props.title}</h1>
+            <h2>{this.props.subtitle}</h2>
             </div>
         );
     }
@@ -34,16 +39,25 @@ class Action extends React.Component {
 
 class Option extends React.Component {
     render() {
-        return <li></li>;
+        return <li key>{this.props.value}</li>;
     }
 }
 
 class Options extends React.Component {
     render() {
+
+        let opcoes;
+        if (!this.props.values) {
+            return '';
+        }
+
         return (
             <ol>
-                <Option />
-                <Option />
+                {
+                    this.props.values.map((valor, indice) => {
+                        return <Option key={indice} value={valor} />;
+                    })
+                }
             </ol>
         );
     };
