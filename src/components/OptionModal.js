e2fc5997cfe8de4aeb33851ffb62cf72d2d5bc62
Rafael Fontoura <rafael.fontoura@gmail.com>
2019-01-02 22:00:47 -0200

    Definido estilos para componente modal
    1. Criando SCSS para estilos do modal
    2. Criando estilos próprios e definido quebra de palavras longas

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import Modal from 'react-modal';
 
-const OptionModal = (props) => (
-    <Modal
-        isOpen={!!props.opcaoSelecionada}
-        onRequestClose={props.onFecharJanela}
-        contentLabel={'Título do Modal'}
-    >
-        <h1>ÉOQ??</h1>
-        {props.opcaoSelecionada && <p>{props.opcaoSelecionada}</p>}
-        <button onClick={props.onFecharJanela}>Belezz</button>
-    </Modal>
-);
+export default class OptionModal extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
-export default OptionModal;
+    render() {
+        Modal.setAppElement(document.getElementById('app'));
+        return (
+            <Modal
+                isOpen={!!this.props.opcaoSelecionada}
+                onRequestClose={this.props.onFecharJanela}
+                closeTimeoutMS={200}
+                className="modal"
+            >
+                <h3 className="modal__title">Opção Selecionada</h3>
+                {this.props.opcaoSelecionada && <p className="modal__body">{this.props.opcaoSelecionada}</p>}
+                <button className="button" onClick={this.props.onFecharJanela}>Okay</button>
+            </Modal>
+        );
+    }
+}
